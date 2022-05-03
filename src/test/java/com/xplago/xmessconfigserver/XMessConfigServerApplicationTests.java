a29f1e13a78e6cc93ea78259f2decba0528aef83
= <nikita.zag.rom@gmail.com>
2022-05-03 14:35:22 +0300

    Initial commit

---
+++ src/test/java/com/xplago/xmessconfigserver/XMessConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xplago.xmessconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XMessConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
